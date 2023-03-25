d93b3e53d932dbd76ec2a1f7657958ee138db24e
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-03-26 01:38:04 +0600

    solution 2 explored

--- stream.js
+++ stream.js
@@ -2,9 +2,18 @@ const fs = require("fs");
 const server = require("http").createServer();
 
 server.on("request", (req, res) => {
-  fs.readFile("test-file.txt", (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
+  //solution 1
+  //   fs.readFile("test-file.txt", (err, data) => {
+  //     if (err) console.log(err);
+  //     res.end(data);
+  //   });
+  //solution 2
+  const readAble = fs.createReadStream("test-file.txt");
+  readAble.on("data", (piece) => {
+    res.write(piece);
+  });
+  readAble.on("end", () => {
+    res.end();
   });
 });
 
